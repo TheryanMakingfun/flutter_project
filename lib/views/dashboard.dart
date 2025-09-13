9ef537c518abc560ca1eb23b597a5bd1e3aea9b4
TheryanMakingfun <gederyan880@gmail.com>
2025-09-13 14:52:42 +0800

    menambahkan new chat, laporkan dan edukasi

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -6,7 +6,7 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     backgroundColor: const Color.fromARGB(255, 250, 201, 250),
+     backgroundColor: const Color.fromARGB(255, 210, 208, 249),
      body: SafeArea(
       child: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
@@ -38,23 +38,100 @@ class Dashboard extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             
-            //Card ke1
-            Card(
-              color: Colors.blue.shade100,
-              elevation: 1,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),),
-              child: const Padding(padding: EdgeInsets.all(16),
-              child: Row(
-                children: [
-                  Icon(Icons.shield, size: 32, color: Colors.blue,),
-                  SizedBox(width: 10,),
-                  Expanded(child: Text("Jangan takut bercerita, karena SABDA ruang amanmu berbagi cerita", style: TextStyle(fontSize: 12),))
-                ],
-              ),),
-            ),
+            //card ke1-INFO
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade50,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                padding: const EdgeInsets.all(16),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(Icons.shield, size: 35, color: Colors.blue),
+                    SizedBox(height: 12),
+                    Text(
+                      "Jangan Takut bercerita, karena SABDA ruang amanmu untuk berbagi cerita",
+                      style: TextStyle(fontSize: 14),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
+                ),
+              ),
 
-            const SizedBox(height: 16,),
+              const SizedBox(height: 16),
+
+              //  New Chat
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade50,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                padding: const EdgeInsets.all(16),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(Icons.chat, size: 30, color: Colors.blue),
+                    SizedBox(height: 12),
+                    Text(
+                      "New Chat",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text("Lorem Ipsum...", style: TextStyle(fontSize: 12, color: Colors.blueGrey),)
+                  ],
+                ),
+              ),
+            
+              const SizedBox(height: 16),
+
+              //row laporkan dan edukasi
+              Row(
+                children: [
+                  Expanded(child: Card(
+                    color: Colors.red.shade100,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                    child: const Padding(padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.send, size: 25, color: Colors.red),
+                        SizedBox(height: 8),
+                        Text("Laporkan", textAlign: TextAlign.start ,style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                        SizedBox(height: 8),
+                        Text("Lorem Ipsum...", textAlign: TextAlign.start ,style: TextStyle(fontSize: 10, color: Colors.blueGrey),)
+                      ],
+                    )),
+                  ),),
+
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: Card(
+                      color: Colors.green.shade100,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(Icons.menu_book, size: 25, color: Colors.green),
+                            SizedBox(height: 8),
+                            Text("Edukasi", textAlign: TextAlign.start, style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+                            SizedBox(height: 8),
+                        Text("Lorem Ipsum...", textAlign: TextAlign.start ,style: TextStyle(fontSize: 10, color: Colors.blueGrey),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+
           ],
         ),
       )), 
